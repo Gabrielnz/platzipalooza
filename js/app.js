3c8b4623c903b48c601d647d447e58e8ee823964
Gabriel Nunez <gabriel.nzn@gmail.com>
2017-06-02 16:46:09 -0400

    Agregado un menu off-canvas para dispositivos pequenos

--- js/app.js
+++ js/app.js
@@ -4,3 +4,11 @@ function navTo(elem) {
 	const panel = $(elem).data('toggleDia')
 	$('#lineup-tabs').foundation('selectTab', panel)
 }
+
+const $offCanvas = $('#offCanvas')
+/* Cuando se seleccione una opcion de navegacion en el menu off-canvas, 
+    cierra el menu automaticamente */
+$offCanvas.find('li').click(ev => {
+
+  $offCanvas.foundation('close')
+})
